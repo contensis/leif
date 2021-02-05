708e1bcaaee9cefdab032055dbadebc768bd92a8
f.beavan <f.beavan@zengenti.com>
2021-02-05 17:18:21 +0000

    fix: lead paragraph and text block stories

--- src/app/components/genericHero/GenericHero.stories.js
+++ src/app/components/genericHero/GenericHero.stories.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { text } from '@storybook/addon-knobs';
 
 import GenericHero from './GenericHero';
 
--- src/app/components/textBlock/TextBlock.stories.js
+++ src/app/components/textBlock/TextBlock.stories.js
@@ -1,33 +1,14 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { text } from '@storybook/addon-knobs';
 
 import TextBlock from './TextBlock';
 
-storiesOf('Features | Global', module).add(
-  'Lead Paragraph | TextBlock',
-  () => {
-    return (
-      <>
-        <TextBlock
-          isLeadParagraph
-          text={text(
-            'Lead Paragraph',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rhoncus interdum praesent tortor, elit vel tortor nunc, amet. Tempor nunc natoque eget amet. '
-          )}
-        />
-        <TextBlock
-          text={text(
-            'Text Block',
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing vitae aliquam volutpat nibh duis enim mi nibh. Orci, molestie nisi, aliquam accumsan, eros, ipsum. Ante velit ullamcorper ultricies morbi. Malesuada ipsum felis, fermentum mauris. Diam mauris ultrices sed nunc, eget neque lectus porttitor. Eget duis iaculis sed pharetra. Viverra risus amet nisl, volutpat vestibulum cursus donec id aliquam. Diam sollicitudin curabitur hac consectetur elementum at. Nisi lectus bibendum aliquam placerat tortor.'
-          )}
-        />
-      </>
-    );
-  },
-  {
-    knobs: {
-      escapeHTML: false,
-    },
-  }
-);
+export default {
+  title: 'Global/Components/TextBlock',
+  component: TextBlock,
+}
+
+const Template = args => <TextBlock {...args} />;
+export const Primary = Template.bind({});
+Primary.args = { 
+  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing vitae aliquam volutpat nibh duis enim mi nibh. Orci, molestie nisi, aliquam accumsan, eros, ipsum. Ante velit ullamcorper ultricies morbi. Malesuada ipsum felis, fermentum mauris. Diam mauris ultrices sed nunc, eget neque lectus porttitor. Eget duis iaculis sed pharetra. Viverra risus amet nisl, volutpat vestibulum cursus donec id aliquam. Diam sollicitudin curabitur hac consectetur elementum at. Nisi lectus bibendum aliquam placerat tortor.',
+};
