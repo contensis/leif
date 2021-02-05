724550aefd4695889c7294950c0f11612a49d2f8
f.beavan <f.beavan@zengenti.com>
2021-02-05 16:50:36 +0000

    fix: quote story

--- src/app/components/quoteBlock/QuoteBlock.stories.js
+++ src/app/components/quoteBlock/QuoteBlock.stories.js
@@ -4,21 +4,14 @@ import { text } from '@storybook/addon-knobs';
 
 import QuoteBlock from './QuoteBlock';
 
-storiesOf('Features | Global', module).add(
-  'Quote Block',
-  () => {
-    return (
-      <QuoteBlock
-        quote={text(
-          'Quote',
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem ridiculus ipsum ac neque, quis morbi. Maecenas ullamcorper iaculis ipsum porttitor. In vitae orci sapien egestas quis senectus.'
-        )}
-      />
-    );
-  },
-  {
-    knobs: {
-      escapeHTML: false,
-    },
-  }
-);
+
+export default {
+  title: 'Global/Components/QuoteBlock',
+  component: QuoteBlock,
+}
+
+const Template = args => <QuoteBlock {...args} />;
+export const Primary = Template.bind({});
+Primary.args = { 
+  quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus lorem ridiculus ipsum ac neque, quis morbi. Maecenas ullamcorper iaculis ipsum porttitor. In vitae orci sapien egestas quis senectus.',
+};
