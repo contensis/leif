d9e1eb57d965aef00a6d2639af0b98ec4c8a4908
f.beavan <f.beavan@zengenti.com>
2021-02-05 16:39:17 +0000

    fix: related content story

--- src/app/components/relatedContent/RelatedContent.stories.js
+++ src/app/components/relatedContent/RelatedContent.stories.js
@@ -1,71 +1,54 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-// import { text } from '@storybook/addon-knobs';
 
-import tempImage from '../genericHero/assets/women-at-desk.png';
 import RelatedContent from './RelatedContent';
 
-storiesOf('Features | Global', module).add(
-  'Related Content',
-  () => {
-    const image = {
-      asset: {
-        altText: 'Cacti',
-        sys: {
-          uri: tempImage,
-        },
+export default {
+  title: 'Global/Components/RelatedContent',
+  component: RelatedContent,
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+        options: [
+          'blog',
+        ]
       },
-    };
-
-    const results = [
-      {
-        title: 'Blog title can span multiple lines of text',
-        text:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non nisl dictumst amet arcunim elit sed consectetur.',
-        image: image,
-        readTime: 2,
-        sys: {
-          contentTypeId: 'blog',
-        },
-        date: new Date().toISOString(),
-      },
-      {
-        title: 'Blog title can span multiple lines of text',
-        text:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non nisl dictumst amet arcunim elit sed consectetur.',
-        image: image,
-        readTime: 2,
-        sys: {
-          contentTypeId: 'blog',
-        },
-        date: new Date().toISOString(),
-      },
-      {
-        title: 'Blog title can span multiple lines of text',
-        text:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non nisl dictumst amet arcunim elit sed consectetur.',
-        image: image,
-        readTime: 2,
-        sys: {
-          contentTypeId: 'blog',
-        },
-        date: new Date().toISOString(),
-      },
-    ];
-
-    const link = {
-      label: 'View all blogs',
-      href: '#',
-      type: 'secondary',
-    };
-
-    return (
-      <RelatedContent title="Related blogs" results={results} link={link} />
-    );
-  },
-  {
-    knobs: {
-      escapeHTML: false,
     },
+  },
+}
+
+const Template = ({type = 'blog', ...args}) => {
+  const result = {
+    title: 'Blog title can span multiple lines of text',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non nisl dictumst amet arcunim elit sed consectetur.',
+    image: {
+      asset: {
+        sys: {
+          uri: 'https://source.unsplash.com/6hgrfOJhr74'
+        }
+      }
+    },
+    sys: {
+      contentTypeId: type,
+    },
+    date: new Date().toISOString(),
+    readTime: 7,
   }
-);
+
+  const results = [];
+  for (let i = 0; results.length < 3; i++) {
+    results.push(result);
+  }
+  return <RelatedContent results={results} {...args} />;
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  title: 'Blog title can span multiple lines of text',
+  text: 'Image accreditation and information can span multiple lines.',
+  link: {
+    label: 'View all blogs',
+    href: '#',
+    type: 'secondary',
+  }
+}
