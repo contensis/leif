3a6e6abbc56eb0c4062f3f016d972cca6b02cb37
f.beavan <f.beavan@zengenti.com>
2021-02-05 15:59:32 +0000

    fix: video player stories

--- src/app/components/videoPlayer/VideoPlayer.stories.js
+++ src/app/components/videoPlayer/VideoPlayer.stories.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-// import { text } from '@storybook/addon-knobs';
 
 import VideoPlayer from './VideoPlayer';
 
-storiesOf('Features | Global', module).add(
-  'Video Player',
-  () => {
-    return (
-      <VideoPlayer
-        title="Bean Time-Lapse - 25 days | Soil cross section"
-        type="YouTube"
-        externalURL="https://youtu.be/w77zPAtVTuI"
-      />
-    );
-  },
-  {
-    knobs: {
-      escapeHTML: false,
+
+export default {
+  title: 'Global/Components/VideoPlayer',
+  component: VideoPlayer,
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+        options: [
+          'YouTube', 
+          'Internal', 
+        ],
+      },
     },
-  }
-);
+  },
+}
+
+const Template = ({ type, ...args }) => <VideoPlayer type={type} {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  title: 'Bean Time-Lapse - 25 days | Soil cross section',
+  type: "YouTube",
+  externalURL: "https://youtu.be/w77zPAtVTuI"
+}
