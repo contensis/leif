8c638eecf72864de8b7374f790a9ccc5150be4ac
JMoo <j.moore@zengenti.com>
2021-02-09 15:39:58 +0000

    wip: blog post component

--- src/app/pages/BlogPost/blogPost.page.js
+++ src/app/pages/BlogPost/blogPost.page.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import Header from '~/components/header/Header'
+import Footer from '~/components/footer/Footer'
+
+import BlogHero from '~/components/genericHero/GenericHero'
+import BlogInformation from '~/components/blogInformation/BlogInformation'
+import LeadParagraph from '~/components/leadParagraph/LeadParagraph'
+import Composer from '~/components/composer/composer'
 const BlogPost = ({ entry }) => (
   <>
     <Header />
-    <h1>Testing</h1>
+    <BlogHero title={entry.entryTitle} image={entry.primaryImage} />
+    <BlogInformation person={entry.author} readTime="2mins" date={entry.sys.version.published} />
+    <LeadParagraph text={entry.leadParagraph} />
+    <Composer fields={entry.postBody} />
+    <Footer />
   </>
 );
 
