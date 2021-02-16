1b283e55f3b799a9a9dab808162f654a67849e97
f.beavan <f.beavan@zengenti.com>
2021-02-16 16:52:23 +0000

    feat: centered lead paragraph

--- src/app/components/leadParagraph/LeadParagraph.styled.tsx
+++ src/app/components/leadParagraph/LeadParagraph.styled.tsx
@@ -1,10 +1,32 @@
 import styled, { css } from 'styled-components';
 
+interface Props {
+  theme: any;
+  type: string;
+}
+
 const LeadParagraphStyled = styled.div`
-  ${({ theme }) => {
+  ${({ theme, type }: Props) => {
     return css`
       ${theme?.typeStyles?.h4};
       font-family: ${theme?.typeStyles?.fontFamily?.headings};
+      ${type === 'center' && css`
+        text-align: center;
+        color: ${theme?.colors?.secondary_light};
+        padding-top: 80px;
+        margin-top: 24px;
+        position: relative;
+        &:before {
+          content: '';
+          position: absolute;
+          top: -24px;
+          left: 50%;
+          transform: translateX(-50%);
+          background-image: url('/static/img/svgs/generic-hero-shape.svg');
+          width: 220px;
+          height: 80px;
+        }
+      `}
     `;
   }}
 `;
--- src/app/components/leadParagraph/LeadParagraph.tsx
+++ src/app/components/leadParagraph/LeadParagraph.tsx
@@ -4,12 +4,14 @@ import LeadParagraphStyled from './LeadParagraph.styled';
 export interface Props {
   className?: string;
   text: string;
+  type?: 'default' | 'center';
 }
 
-const LeadParagraph = ({ className, text }: Props) => {
+const LeadParagraph = ({ className, type = "default", text }: Props) => {
   return (
     <LeadParagraphStyled
       className={className}
+      type={type}
       dangerouslySetInnerHTML={{ __html: text }}
     />
   );
