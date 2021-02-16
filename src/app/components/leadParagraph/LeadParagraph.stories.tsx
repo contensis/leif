816ba80b3aa2a2fe6321c9d50e3e5c98f1089028
f.beavan <f.beavan@zengenti.com>
2021-02-16 12:07:07 +0000

    fix: lead paragraph story as tsx

---
+++ src/app/components/leadParagraph/LeadParagraph.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import LeadParagraph, { Props } from './LeadParagraph';
+
+export default {
+  title: 'Global/Components/LeadParagraph',
+  component: LeadParagraph,
+} as Meta;
+
+const Template: Story<Props> = args => <LeadParagraph {...args} />;
+export const Primary = Template.bind({});
+Primary.args = {
+  text:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rhoncus interdum praesent tortor, elit vel tortor nunc, amet. Tempor nunc natoque eget amet. ',
+};
--- src/app/components/leadParagraph/LeadParagraph.tsx
+++ src/app/components/leadParagraph/LeadParagraph.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import LeadParagraphStyled from './LeadParagraph.styled';
-interface Props {
+export interface Props {
   className?: string;
   text: string;
 }
