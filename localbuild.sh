2f8a20684bc35969e1b5fa078b9f9b2fe5811351
richard-chivers <r.chivers@zengenti.com>
2021-03-17 13:10:52 +0000

    Fix: Temporary fix for immutable dependancy issue.

--- localbuild.sh
+++ localbuild.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 docker build --force-rm -t zen-local-app -f docker/nodebuilder.DockerFile .
 docker build -t zen-local-app-server --build-arg builder_image=zen-local-app -f docker/ci-build.DockerFile .
-
-#Build & Run HA Proxy
-
-docker build -t zen-app-test-haproxy -f docker/haproxy.DockerFile .
-
-# Run Haproxy 
-
-#docker run -it -p 3002:3002 zen-app-test-haproxy
+docker run -e alias=zenhub -e projectId=leif -it -p 3001:3001 zen-local-app-server
