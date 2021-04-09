2601846cfcaf20d488f7c1b93ef1ae3e746de7c6
f.beavan <f.beavan@zengenti.com>
2021-04-09 14:21:35 +0100

    chore: clean up filters file

--- src/app/components/search/filters/index.ts
+++ src/app/components/search/filters/index.ts
@@ -4,12 +4,12 @@ import {
   ProductFilters as Product,
 } from '../../../core/schema';
 
+// Blog Listing Filters
 export const BlogFilters = {
   [Blog.category]: {
     title: 'Browse by category',
     contentTypeId: ContentTypes.category,
     fieldId: 'category.sys.id',
-    isSelected: false,
     isSingleSelect: true,
     items: [],
   },
@@ -17,29 +17,26 @@ export const BlogFilters = {
     title: 'Browse by author',
     contentTypeId: ContentTypes.person,
     fieldId: 'author.sys.id',
-    isSelected: false,
     isSingleSelect: true,
     items: [],
   },
 };
 
+// Product Listing Filters
 export const ProductFilters = {
   [Product.contentTypeId]: {
     title: 'Browse product types',
     contentTypeId: [ContentTypes.pot, ContentTypes.plant],
     fieldId: 'sys.contentTypeId',
-    isSelected: false,
     isSingleSelect: true,
     items: [
       {
         key: 'pot',
         title: 'Pot',
-        isSelected: false,
       },
       {
         key: 'plant',
         title: 'Plant',
-        isSelected: false,
       },
     ],
   },
@@ -47,7 +44,6 @@ export const ProductFilters = {
     title: 'Browse plant types',
     contentTypeId: ContentTypes.plantType,
     fieldId: 'type.sys.id',
-    isSelected: false,
     isSingleSelect: true,
     items: [],
   },
@@ -55,23 +51,19 @@ export const ProductFilters = {
     title: 'Browse by colour',
     contentTypeId: ContentTypes.pot,
     fieldId: 'colour',
-    isSelected: false,
     isSingleSelect: true,
     items: [
       {
         key: 'white',
         title: 'White',
-        isSelected: false,
       },
       {
         key: 'grey',
         title: 'Grey',
-        isSelected: false,
       },
       {
         key: 'brown',
         title: 'Brown',
-        isSelected: false,
       },
     ],
   },
@@ -79,24 +71,20 @@ export const ProductFilters = {
     title: 'Browse by pot size',
     contentTypeId: ContentTypes.pot,
     fieldId: 'potVariant.externalHeightCM',
-    isSelected: false,
     isSingleSelect: true,
     fieldOperator: 'between',
     items: [
       {
-        key: '0,30',
+        key: '0-30',
         title: 'Small',
-        isSelected: false,
       },
       {
-        key: '31,60',
+        key: '31-60',
         title: 'Medium',
-        isSelected: false,
       },
       {
-        key: '61,2000000',
+        key: '61-2000000',
         title: 'Large',
-        isSelected: false,
       },
     ],
   },
@@ -104,24 +92,20 @@ export const ProductFilters = {
     title: 'Browse by plant size',
     contentTypeId: ContentTypes.plant,
     fieldId: 'plantVariant.heightCM',
-    isSelected: false,
     isSingleSelect: true,
     fieldOperator: 'between',
     items: [
       {
         key: '0-30',
         title: 'Small',
-        isSelected: false,
       },
       {
         key: '31-60',
         title: 'Medium',
-        isSelected: false,
       },
       {
         key: '61-2000000',
         title: 'Large',
-        isSelected: false,
       },
     ],
   },
