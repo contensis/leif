85afff72e7c6ecfef885c1b019b751a7ff33fce0
f.beavan <f.beavan@zengenti.com>
2021-04-16 09:04:54 +0100

    fix: two tweaks to calculate read time function

--- src/app/utils/calculateReadTime.ts
+++ src/app/utils/calculateReadTime.ts
@@ -4,6 +4,14 @@ interface Props {
   imageArr: string[];
 }
 
+// Dictionary of all the fields id's from the composer.
+const FIELDS_DICTIONARY = {
+  MARKUP: 'markup',
+  BLOCK_QUOTE: 'blockQuote',
+  IMAGE: 'image',
+  FEATURED_PRODUCT: 'featuredProduct+',
+};
+
 // Get all the text and images and put them into their own array's.
 const _getTextAndImages = (composer: any) => {
   const textArr: string[] = [];
@@ -11,17 +19,17 @@ const _getTextAndImages = (composer: any) => {
   composer.map((item: any) => {
     const { _type, title, text, quote, caption } = item || {};
     switch (_type) {
-      case 'markup':
+      case FIELDS_DICTIONARY.MARKUP:
         textArr.push(text);
         break;
-      case 'blockQuote':
+      case FIELDS_DICTIONARY.BLOCK_QUOTE:
         textArr.push(quote);
         break;
-      case 'image':
+      case FIELDS_DICTIONARY.IMAGE:
         imageArr.push('image');
         textArr.push(caption);
         break;
-      case 'featuredProduct':
+      case FIELDS_DICTIONARY.FEATURED_PRODUCT:
         textArr.push(title);
         textArr.push(text);
         break;
@@ -32,12 +40,14 @@ const _getTextAndImages = (composer: any) => {
   return { textArr, imageArr };
 };
 
+// Format the time into a human readable format
 const _getHumanTime = (time: number) => {
   if (time < 0.5) return 'less than a minute';
   if (time >= 0.5 && time < 1.5) return '1 min';
   return `${Math.ceil(time)} min`;
 };
 
+// Calculates the image read time
 const _getImageReadTime = (count: number) => {
   const IMAGE_READ_TIME = 12;
   let seconds = 0;
@@ -49,24 +59,29 @@ const _getImageReadTime = (count: number) => {
   return seconds / 60;
 };
 
-export const _calReadTime = (composer: any) => {
+// Calculates the words read time
+const _getWordReadTime = (textArr: string[]) => {
+  if (!textArr || textArr.length < 1) return 0;
   const WORDS_PER_MIN = 275;
+  let words = 0;
+  textArr.map((text: string) => {
+    return (words += text.trim().split(/\s+/).length);
+  });
 
+  const wordTime: number = Math.ceil(words / WORDS_PER_MIN);
+  return wordTime;
+};
+
+// Calculates the overall read time combing both words and images
+export const _calReadTime = (composer: any) => {
   const TEXT_AND_IMAGES_OBJECT: Props = _getTextAndImages(composer);
   const { textArr, imageArr } = TEXT_AND_IMAGES_OBJECT || [];
 
-  // TODO: can this be moved into its own component?
   if (textArr) {
-    let words = 0;
-    // Map through text array adding to the total words
-    textArr.map((text: string) => {
-      return (words += text.trim().split(/\s+/).length);
-    });
-
     let imagesCount = imageArr && imageArr.length;
     const imageTime = _getImageReadTime(imagesCount);
+    const wordTime = _getWordReadTime(textArr);
 
-    const wordTime = Math.ceil(words / WORDS_PER_MIN);
     return _getHumanTime(imageTime + wordTime);
   }
 };
