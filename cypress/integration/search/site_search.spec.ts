56941de46f8535b2126669ccaa67762f63169b85
JMoo <j.moore@zengenti.com>
2021-04-20 10:25:27 +0100

    refactor: seperate live and site search tests

--- cypress/integration/search/site_search.spec.ts
+++ cypress/integration/search/site_search.spec.ts
@@ -1,11 +1,8 @@
-describe('Test that a user can search for content.', () => {
-  it('Searches for the term and, and checks that results are returned both by the live search and when clicking though to the results.', () => {
+describe('Test that a user can search for content site wide.', () => {
+  it('Searches for the term and, and checks that results are returned by the site search when clicking though to the results.', () => {
     cy.visit('/');
     cy.get('.header__search-icon').click();
     cy.get('.header-search__input').type('and');
-    cy.get('.header-search__results')
-      .children('[class*="SearchCardStyled"]')
-      .should('have.length.gt', 1);
     cy.get('.header-search__site-search--link').click();
     cy.url().should('include', '/search?term=and');
     cy.get('.search__results')
