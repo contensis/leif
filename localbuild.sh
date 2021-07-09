444c4e0ab3487a7891a291b49dec5cf9bc440213
JMoo <j.moore@zengenti.com>
2021-07-09 16:09:52 +0100

    fix: updates to try and reduce container size

--- localbuild.sh
+++ localbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build --force-rm -t zen-local-app -f docker/nodebuilder.DockerFile .
-docker build -t zen-local-app-server --build-arg builder_image=zen-local-app -f docker/ci-build.DockerFile .
-docker run -e alias=zenhub -e projectId=leif -it -p 3001:3001 zen-local-app-server
+docker build --force-rm -t leif-local-app -f docker/nodebuilder.DockerFile .
+docker build -t leif-local-app-server --build-arg builder_image=leif-local-app -f docker/ci-build.DockerFile .
+docker run -e alias=zenhub -e projectId=leif -it -p 3001:3001 leif-local-app-server
