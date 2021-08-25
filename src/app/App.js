2c827812c2c172e48ab896255637e75019804862
f.beavan <f.beavan@zengenti.com>
2021-08-25 08:56:15 +0100

    fix: attempt to fix double loading

--- src/app/App.js
+++ src/app/App.js
@@ -1,28 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
 import { ThemeProvider } from 'styled-components';
 import { RouteLoader } from '@zengenti/contensis-react-base/routing';
-import { Loading } from '~/core/routes/Loading';
 import NotFound from '~/pages/NotFound/NotFound.page';
-import { selectRouteLoading } from './core/redux/selectors';
 
 import GlobalStyle from '~/theme/global/globalStyles';
 import { defaultTheme } from './theme/leif';
 
 const AppRoot = props => {
-  const stateLoading = useSelector(selectRouteLoading);
-  const [isLoading, setIsLoading] = useState(stateLoading);
-  useEffect(() => {
-    setIsLoading(stateLoading);
-  }, [stateLoading]);
   return (
     <>
       <div id="app-root">
         <ThemeProvider theme={defaultTheme}>
           <GlobalStyle />
-          {isLoading && <Loading {...props} />}
           <RouteLoader {...props} notFoundComponent={NotFound} />
         </ThemeProvider>
       </div>
