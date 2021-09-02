949baef555995381b5894f73b0b789afba8ef53f
f.beavan <f.beavan@zengenti.com>
2021-09-02 10:00:00 +0100

    chore: comment added to explain to users what they will need to do *currently* to update the 404 page for their site.

--- src/app/App.js
+++ src/app/App.js
@@ -9,6 +9,18 @@ import GlobalStyle from '~/theme/global/globalStyles';
 import { defaultTheme } from './theme/leif';
 
 const AppRoot = props => {
+  /*
+    *notFoundComponent={NotFound}*
+    This Leif 404 Page is for local development purposes only. To see this working on your live site, you will need to add this to the load balancer, to do this, please follow the steps below. 
+
+    1. Go to Contensis.com.
+    2. Login and go to the help desk.
+    1. Raise a new support request.
+    2. Complete the form and give details of what error page you'd like to add.
+    3. Upload the error page html file.
+    4. Submit your request.
+  */
+
   return (
     <>
       <div id="app-root">
