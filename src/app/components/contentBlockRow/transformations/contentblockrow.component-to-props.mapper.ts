8412c59757c138920f5ff82c67ceee3dce962d89
nflatley-zengenti <n.flatley@zengenti.com>
2021-09-24 18:13:56 +0100

    fix: content block row mapping error

--- src/app/components/contentBlockRow/transformations/contentblockrow.component-to-props.mapper.ts
+++ src/app/components/contentBlockRow/transformations/contentblockrow.component-to-props.mapper.ts
@@ -1,18 +1,35 @@
+import mapJson from 'jsonpath-mapper';
+
 export const contentBlockRowPropsMapping = {
   results: {
     $path: '.',
     $formatting: (block: any) => {
-      return {
-        type: 'content',
-        title: block.title,
-        imageUri:
-          block.image &&
-          block.image.asset &&
-          block.image.asset.sys &&
-          block.image.asset.sys.uri,
-        imageAlt: block.image && block.image.altText,
-        text: block.text,
-      };
+      return Object.values(block)
+        .map(
+          (cb: any) =>
+            cb &&
+            mapJson(cb, {
+              type: () => 'content',
+              title: 'title',
+              imageUri: 'image.asset.sys.uri',
+              imageAlt: 'image.altText',
+              text: 'text',
+            })
+        )
+        .filter(cb => cb);
     },
+    // $formatting: (block: any) => {
+    //   return {
+    //     type: 'content',
+    //     title: block.title,
+    //     imageUri:
+    //       block.image &&
+    //       block.image.asset &&
+    //       block.image.asset.sys &&
+    //       block.image.asset.sys.uri,
+    //     imageAlt: block.image && block.image.altText,
+    //     text: block.text,
+    //   };
+    // },
   },
 };
