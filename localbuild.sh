afd6321a8ce27f01c42ebc28ce29360f52ee7904
richard-chivers <r.chivers@zengenti.com>
2022-11-24 13:20:03 +0000

    fix: minor issues in relation to file renaming.

--- localbuild.sh
+++ localbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build --force-rm -t leif-local-app -f docker/nodebuilder.DockerFile .
-docker build -t leif-local-app-server --build-arg builder_image=leif-local-app -f docker/ci-build.DockerFile .
+docker build --force-rm -t leif-local-app -f docker/nodebuilder.Dockerfile .
+docker build -t leif-local-app-server --build-arg builder_image=leif-local-app -f docker/ci-build.Dockerfile .
 docker run -e alias=leif -e projectId=website -it -p 3001:3001 leif-local-app-server
