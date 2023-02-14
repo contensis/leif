dfb2ee360f9a23507124b22954945fbf876a9c89
Felicity Beavan <f.beavan@zengenti.com>
2023-02-14 16:58:20 +0000

    fix: content block mapping

--- src/app/components/contentBlockRow/transformations/contentblockrow.component-to-props.mapper.ts
+++ src/app/components/contentBlockRow/transformations/contentblockrow.component-to-props.mapper.ts
@@ -3,33 +3,13 @@ import mapJson from 'jsonpath-mapper';
 export const contentBlockRowPropsMapping = {
   results: {
     $path: '.',
-    $formatting: (block: any) => {
-      return Object.values(block)
-        .map(
-          (cb: any) =>
-            cb &&
-            mapJson(cb, {
-              type: () => 'content',
-              title: 'title',
-              imageUri: 'image.asset.sys.uri',
-              imageAlt: 'image.altText',
-              text: 'text',
-            })
-        )
-        .filter(cb => cb);
-    },
-    // $formatting: (block: any) => {
-    //   return {
-    //     type: 'content',
-    //     title: block.title,
-    //     imageUri:
-    //       block.image &&
-    //       block.image.asset &&
-    //       block.image.asset.sys &&
-    //       block.image.asset.sys.uri,
-    //     imageAlt: block.image && block.image.altText,
-    //     text: block.text,
-    //   };
-    // },
+    $formatting: (cb: any) =>
+      mapJson(cb, {
+        type: () => 'content',
+        title: 'title',
+        imageUri: 'image.asset.sys.uri',
+        imageAlt: 'image.altText',
+        text: 'text',
+      }),
   },
 };
